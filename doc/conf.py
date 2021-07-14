d3f754ab0294bbf97635734b8d11bd292d6286d7
Ross Barnowski <rossbar@berkeley.edu>
2021-07-14 14:31:40 +0300

    Add myst-nb extension.

--- doc/conf.py
+++ doc/conf.py
@@ -28,6 +28,7 @@ author = 'Ross Barnowski'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    "myst_nb",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
